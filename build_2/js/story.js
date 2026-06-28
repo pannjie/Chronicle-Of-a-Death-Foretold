29922123205fbfd8406eb8bfb50946e74f3f3c49
pjpjpj <98586016+pannjie@users.noreply.github.com>
2026-06-28 12:46:55 +0100

    28 Jun

--- build_2/js/story.js
+++ build_2/js/story.js
@@ -1,18 +1,23 @@
 import {gsap} from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import {ScrollToPlugin} from 'gsap/ScrollToPlugin';
 import * as d3 from 'd3';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 gsap.utils.toArray('.step').forEach((el, i) => {
   gsap.from(el, {
     opacity: 0.8,
     y: 40,
-    duration: 0.8,
+    ease: 'none',
+    immediateRender: false,
     scrollTrigger: {
       trigger: el,
       start: 'top 80%',
+      end: 'top 35%',
       scrub: 1.5,
+      // toggleActions: 'play none none none',
+
     },
   });
 });
@@ -22,22 +27,59 @@ gsap.utils.toArray('.step').forEach((el, i) => {
     trigger: el,
     start: 'top center',
     end: 'bottom center',
-    onEnter:     () => { showVideo(i); showWallets(i); },
-    onEnterBack: () => { showVideo(i); showWallets(i); },
+    onEnter:     () => { showVideo(i, 'down'); showWallets(i); },
+    onEnterBack: () => { showVideo(i, 'up');   showWallets(i); },
   });
 });
 
-function showVideo(i) {
-  gsap.to('[data-visual]', { opacity: 0, duration: 0.8, ease: 'power2.inOut' });
-  gsap.to(`[data-visual="${i}"]`, { opacity: 1, duration: 0.8, ease: 'power2.inOut' });
+// on Enter ; stagger video appearing, autoscroll wallet list, pause when wallet is hovered
+// on Enterback ; do the same 
 
-  if (i === 5) {
+// if (i === 2) {
+  //   gsap.from(')}
+
+  const walletScroll = gsap.to('.wallet-list', {
+    y: '-50%',
+    ease: 'none',
+    duration: 20,
+    paused: true,
+  }
+  );
+
+
+
+
+
+function showVideo(i, direction) {
+  gsap.to('[data-visual]', { opacity: 0, duration: 0.8, ease: 'power2.inOut', overwrite: 'auto' });
+  gsap.to(`[data-visual="${i}"]`, { opacity: 1, duration: 0.8, ease: 'power2.inOut', overwrite: 'auto' });
+
+  if (i === 1) {
+    gsap.killTweensOf('.all-videos > div');
+    if (direction === 'down') {
+      gsap.from('.all-videos > div', {
+        opacity: 0,
+        duration: 0.5,
+        ease: 'power2.inOut',
+        stagger: { grid: [4,4], from: 'random', ease: 'power2.in', amount: 3},
+        overwrite: 'auto',
+      });
+    } else {
+      gsap.set('.all-videos > div', { opacity: 1 });
+    }
+  }
+
+  if (i === 4 && direction === 'down') {
+    // const cols = 4;
+    // const rows = Math.ceil(document.querySelectorAll('.wallet-card').length / cols);
     gsap.from('.wallet-card', {
       duration: 0.5,
       scale: 0.1,
       y: 40,
-      ease: 'power2.inOut',
-      stagger: { grid: [7, 15], from: 'center', ease: 'power2.in', amount: 0.5 },
+      // ease: 'power2.inOut',
+      // don't change what's not broken please
+      stagger: { grid: [7, 15], from: 'center', ease: 'power2.in', amount: 1 },
+      overwrite: 'auto',
       onComplete: function() {
         gsap.set('.wallet-card', { clearProps: 'transform' });
         this.targets().forEach(el => {
@@ -49,11 +91,14 @@ function showVideo(i) {
 }
 
 function showWallets(i) {
-  if (i >= 5 && i <= 7) {
+  if (i >= 4 && i <= 8) {
     gsap.to('[data-visual="5"]', { opacity: 1, duration: 0.8, ease: 'power2.inOut' });
   } else {
     gsap.to('[data-visual="5"]', { opacity: 0, duration: 0.8, ease: 'power2.inOut' });
   }
+
+  if (i === 5) {walletScroll.play();}
+  if (i < 5) {walletScroll.pause(0);}
 }
 
 const hoverEls = document.querySelectorAll('.hover-animate');
@@ -68,3 +113,5 @@ hoverEls.forEach(el => {
     gsap.to(el, { backgroundColor: 'rgba(10, 228, 72, 0)', color: 'white', duration: 0.3, ease: 'power2.inOut' });
   });
 });
+
+
